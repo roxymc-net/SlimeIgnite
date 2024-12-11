f9bb3c3229ae6976bc88390052b5ca0351555318
GliczDev <67753196+GliczDev@users.noreply.github.com>
2024-12-11 21:08:00 +0100

    Minimize fat jar

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -7,7 +7,16 @@ val minecraftVersion = project.property("minecraft-version")
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:$minecraftVersion-R0.1-SNAPSHOT")
-    api("net.roxymc.slimeloader:slime-loader:1.0-SNAPSHOT")
+
+    api("net.roxymc.slimeloader:slime-loader:1.0-SNAPSHOT") {
+        isTransitive = false
+    }
+
+    // remember to always keep these up-to-date with slime-loader
+    api("net.kyori:adventure-nbt:4.17.0") {
+        isTransitive = false
+    }
+    api("com.github.luben:zstd-jni:1.5.6-6")
 }
 
 java {
